7ea9935d39a897c20aabb264f4116936c1a6b2c9
KostaKapetanos <konstantinos.kapetanos@student.adelaide.edu.au>
2025-06-20 08:24:42 +0000

    [automatic commit] 2025-06-20 08:24:42 UTC 129.127.202.110

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -62,6 +62,7 @@ router.post('/login', async (req, res) => {
 router.post('/logout', (req, res) => {
   req.session.destroy(err => {
     if (err) return res.status(500).json({ error: 'Logout failed' });
+    res.clearCookie('connect.sid'); // remove session cookie
 
 
 
