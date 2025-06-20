f0270a10204a591c4265a2142e840b1834bb3315
KostaKapetanos <konstantinos.kapetanos@student.adelaide.edu.au>
2025-06-20 04:00:33 +0000

    [automatic commit] 2025-06-20 04:00:33 UTC 129.127.202.110

--- part1/app.js
+++ part1/app.js
@@ -2,9 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql2/promise');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+const dogsRoute = require('./routes/api/dogs');
+const walkRequestsRoute = require('./routes/api/walkrequests');
+const walkersRoute = require('./routes/api/walkers');
 
 var app = express();
 
@@ -16,5 +21,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+let db;
+
+(async () => {
+  try {
+    db = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'DogWalkService'
+    });
+
+    console.log('Connected to DogWalkService database');
+
+    app.use('/api/dogs', dogsRoute(db));
+    app.use('/api/walkrequests', walkRequestsRoute(db));
+    app.use('/api/walkers', walkersRoute(db));
+
+  } catch (err) {
+    console.error('Database connection failed:', err.message);
+  }
+})();
 
 module.exports = app;
